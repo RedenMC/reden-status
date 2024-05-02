2bb35af6b03bb5244938774273b6ad14a6a5d73f
Liyan Zhao <return65535@qq.com>
2024-05-02 17:22:21 +0800

    update

---
+++ status.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+import requests
+import json
+
+if __name__ == "__main__":
+    res = requests.get("https://redenmc.com/api/status")
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    if os.path.exists("data/status.json"):
+        status = json.load(open("data/status.json"))
+    else:
+        status = {}
+    time_now = int(time.time() * 1000)
+    yymmddhhmm = time.strftime("%y%m%d%H%M", time.localtime(time.time()))
+    status[yymmddhhmm] = {
+        "timestamp": time_now,
+        "status": res.status_code,
+        "online_count": res.json()["online"]
+    }
+    json.dump(status, open("data/status.json", "w"))
+    print("Status updated. Time: " + yymmddhhmm + " Status: " + str(res.status_code) + " Online: "
+          + str(res.json()["online"]) + " players.")
+
