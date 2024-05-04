6dda13bbe7abfb60582b1ddd3b16d7a1339a18c4
Liyan Zhao <return65535@qq.com>
2024-05-04 18:16:27 +0800

    Update status.py

--- status.py
+++ status.py
@@ -13,22 +13,22 @@ if __name__ == "__main__":
         status = {}
     time_now = int(time.time() * 1000)
     yymmddhhmm = time.strftime("%Y-%m-%dT%H:%M", time.localtime(time.time()))
-  try:
-    res = requests.get("https://redenmc.com/api/status")
-    status[yymmddhhmm] = {
-        "timestamp": time_now,
-        "status": res.status_code,
-        "online_count": res.json()["online"]
-    }
-    json.dump(status, open("data/status.json", "w"))
-    print("Status updated. Time: " + yymmddhhmm + " Status: " + str(res.status_code) + " Online: "
-          + str(res.json()["online"]) + " players.")
-  except Exception as e:
-    status[yymmddhhmm] = {
-        "timestamp": time_now,
-        "status": 0,
-        "online_count": -1
-    }
-    json.dump(status, open("data/status.json", "w"))
-    print("Network failure.", e)
+    try:
+        res = requests.get("https://redenmc.com/api/status")
+        status[yymmddhhmm] = {
+            "timestamp": time_now,
+            "status": res.status_code,
+            "online_count": res.json()["online"]
+        }
+        json.dump(status, open("data/status.json", "w"))
+        print("Status updated. Time: " + yymmddhhmm + " Status: " + str(res.status_code) + " Online: "
+              + str(res.json()["online"]) + " players.")
+    except Exception as e:
+        status[yymmddhhmm] = {
+            "timestamp": time_now,
+            "status": 0,
+            "online_count": -1
+        }
+        json.dump(status, open("data/status.json", "w"))
+        print("Network failure.", e)
     
