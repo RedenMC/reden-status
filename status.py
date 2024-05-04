e80ab9e4848006bf6b221f78341c748e338e2bf9
Liyan Zhao <return65535@qq.com>
2024-05-04 18:15:39 +0800

    Update status.py

--- status.py
+++ status.py
@@ -5,7 +5,6 @@ import requests
 import json
 
 if __name__ == "__main__":
-    res = requests.get("https://redenmc.com/api/status")
     if not os.path.exists("data"):
         os.mkdir("data")
     if os.path.exists("data/status.json"):
@@ -14,6 +13,8 @@ if __name__ == "__main__":
         status = {}
     time_now = int(time.time() * 1000)
     yymmddhhmm = time.strftime("%Y-%m-%dT%H:%M", time.localtime(time.time()))
+  try:
+    res = requests.get("https://redenmc.com/api/status")
     status[yymmddhhmm] = {
         "timestamp": time_now,
         "status": res.status_code,
@@ -22,4 +23,12 @@ if __name__ == "__main__":
     json.dump(status, open("data/status.json", "w"))
     print("Status updated. Time: " + yymmddhhmm + " Status: " + str(res.status_code) + " Online: "
           + str(res.json()["online"]) + " players.")
-
+  except Exception as e:
+    status[yymmddhhmm] = {
+        "timestamp": time_now,
+        "status": 0,
+        "online_count": -1
+    }
+    json.dump(status, open("data/status.json", "w"))
+    print("Network failure.", e)
+    
